fa2dc5cf21fa18991270160dde4ecb7cd9d73cd1
yuii0614 <2201382@s.asojuku.ac.jp>
2024-01-10 10:47:06 +0900

    ファイル作成

---
+++ src/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1517342-final';
+    const USER = 'LAA1517342';
+    const PASS = 'Pass0614';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
