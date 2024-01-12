1f9f02d76d4a5e6d2ddacfe09d5f4bde7cfa12d6
yuii0614 <2201382@s.asojuku.ac.jp>
2024-01-12 12:12:49 +0900

    登録　改善

--- src/toroku-output.php
+++ src/toroku-output.php
@@ -2,14 +2,17 @@
 <?php require 'db-connect.php'; ?>
 
 <?php
+    $pdo=new PDO($connect, USER, PASS );
 
-    $pdo=new PDO($connect,USER,PASS);
-    $sql=$pdo->prepare('insert into Music values(null,?,?,?)');
-    $sql->execute(
-        [ $_POST['m_name'], $_POST['a_name'], $_POST['genre'] ]
-    );  
-    echo '楽曲を登録しました';
-        
+    if( empty($sql->fetchALL()) ){
+        $sql=$pdo->prepare('insert into customer values(null,?,?,?)');
+        $sql->execute([
+            $_POST['m_name'], $_POST['a_name'], $_POST['genre']
+        ]);
+        echo '楽曲を登録しました。';
+    }else{
+        echo '';
+    }
 ?>
 
 <?php require 'footer.php'; ?>
