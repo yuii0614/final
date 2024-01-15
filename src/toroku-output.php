813b4922cf1c50f162fdde59a317c5c642c61853
yuii0614 <2201382@s.asojuku.ac.jp>
2024-01-15 10:07:43 +0900

    登録　改善

--- src/toroku-output.php
+++ src/toroku-output.php
@@ -3,15 +3,9 @@
 
 <?php
     $pdo=new PDO($connect, USER, PASS);
-    $sql=$pdo->prepare('insert into Music values (null, ?, ?, ?)');
+    $sql=$pdo->prepare('insert into Music values(null, ?, ?, ?)');
     
-    if(empty($_POST['mname'])){
-        echo '曲名を入力してください';
-    }else if(empty($_POST['aname'])){
-        echo '歌手名を入力してください';
-    }else if(empty($_POST['genre_id'])){
-        echo 'ジャンルを選択してください';
-    }else if($sql->execute([ $_POST['mname'], $_POST['aname'], $_POST['genre'] ])){
+    if($sql->execute([ $_POST['mname'], $_POST['aname'], $_POST['genre'] ])){
         echo '楽曲を登録しました！';
     }else{
         echo '楽曲の追加に失敗しました・・・';
