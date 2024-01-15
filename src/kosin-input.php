b961297f21c3aab63579727a63f080209658293b
yuii0614 <2201382@s.asojuku.ac.jp>
2024-01-15 14:58:14 +0900

    更新

--- src/kosin-input.php
+++ src/kosin-input.php
@@ -1,6 +1,5 @@
-<?php require 'header.php'; ?>
 <?php require 'db-connect.php'; ?>
-
+<?php require 'header.php'; ?>
 
 <h1>曲を更新</h1>
 <a href="index.php">メニューに戻る</a>
@@ -15,27 +14,24 @@
 <?php
     $pdo=new PDO($connect, USER, PASS);
 
-    foreach($pdo->query('select * from Music') as $row){
-
-        echo '<form action="kosin-output.php" method="post">';
-        echo '<input type="hidden" name="music_id" value="', $row['music_id'], '">';
-        echo '<div class="td0">', $row['music_id'], '</div>';
+	foreach ($pdo->query('select * from Music') as $row) {
+		echo '<form action="kosin-output.php" method="post">';
+		echo '<input type="hidden" name="music_id" value="', $row['music_id'], '">';
+		echo '<div class="td0">', $row['music_id'], '</div> ';
+		echo '<div class="td1">';
+		echo '<input type="text" name="music_name" value="', $row['music_name'], '">';
+		echo '</div> ';
+		echo '<div class="td1">';
+		echo '<input type="text" name="artist_name" value="', $row['artist_name'], '">';
+		echo '</div> ';
         echo '<div class="td1">';
-        echo '<input type="text" name="music_name" value="', $row['music_name'], '">';
-        echo '</div>';
-        echo '<div class="td1">';
-        echo '<input type="text" name="artist_name" value="', $row['artist_name'], '">';
-        echo '</div>';
-        echo '<div class="td1">';
-        echo '<input type="text" name="genre_id" value="', $row['genre_id'], '">';
-        echo '</div>';
+		echo '<input type="text" name="genre_id" value="', $row['genre_id'], '">';
+		echo '</div> ';
 
-        echo '<div class="td2">';
-        echo '<input type="submit" value="更新"></div>';
-        echo '</form>';
-        echo "\n";
-    }
-
+		echo '<div class="td2"><input type="submit" value="更新"></div>';
+		echo '</form>';
+		echo "\n";
+	}
 ?>
 
 <?php require 'footer.php'; ?>
--- src/kosin-output.php
+++ src/kosin-output.php
@@ -1,7 +1,6 @@
 <?php require 'header.php'; ?>
 <?php require 'db-connect.php'; ?>
 
-
 <h1>曲を更新</h1>
 <a href="index.php">メニューに戻る</a>
 <hr>
@@ -9,33 +8,34 @@
 
 <?php
     $pdo=new PDO($connect, USER, PASS);
-    $sql=$pdo->prepare('update Music set music_name=?, artist_name=?, genre_id=?');
+    $sql=$pdo->prepare('update Music set music_name=?, artist_name=?, genre_id=? where id=?');
 
-    if(empty($_POST['music_name'])){
-        echo '曲名を入力してください';
-    }else if(empty($_POST['artist_name'])){
-        echo '歌手名を入力してください';
-    }else if(empty($_POST['genre_id'])){
-        echo 'ジャンルを選択してください';
-    }else if($sql->execute([htmlspecialchars($_POST['music_name']), $_POST['artist_name'], $_POST['genre_id'] ])){
-        echo '楽曲を更新しました！';
-    }else{
-        echo '楽曲の更新に失敗しました・・・';
+    if (empty($_POST['music_name'])) {
+        echo '曲名を入力してください。';
+    }else if(empty($_POST['music_name'])) {
+        echo '歌手名を入力してください。';
+    }else if( $sql->execute([htmlspecialchars($_POST['music_name']), $_POST['artist_name'], $_POST['genre_id'], $_POST['music_id']]) ){
+        echo '更新に成功しました！';
+    }
+    else{
+        echo '更新に失敗しました・・・';
     }
 ?>
+        <hr>
+        <table>
+        <tr><th>楽曲ID</th><th>曲名</th><th>歌手名</th><th>ジャンル</th></tr>
 
-<table>
-    <tr><th>楽曲ID</th><th>曲名</th><th>歌手名</th><th>ジャンル</th></tr>
-
-    <?php
-        foreach ($pdo->query('select * from Music') as $row) {
-            echo '<tr>';
-            echo '<td>', $row['music_id'], '</td>';
-            echo '<td>', $row['music_name'], '</td>';
-            echo '<td>', $row['artist_name'], '</td>';
-            echo '<td>', $row['genre_id'], '</td>';
-            echo '</tr>';
-            echo "\n";
-        }
-    ?>
+<?php
+foreach ($pdo->query('select * from Music') as $row) {
+    echo '<tr>';
+    echo '<td>', $row['music_id'], '</td>';
+    echo '<td>', $row['music_name'], '</td>';
+    echo '<td>', $row['artist_name'], '</td>';
+    echo '<td>', $row['genre_id'], '</td>';
+    echo '</tr>';
+    echo "\n";
+}
+?>
 </table>
+
+<?php require 'footer.php'; ?>
