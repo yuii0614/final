d0949db1fcc1fd96fb7a28cb9cee56eb89ac7c6a
yuii0614 <2201382@s.asojuku.ac.jp>
2024-01-16 09:48:57 +0900

    更新

--- src/kosin-output.php
+++ src/kosin-output.php
@@ -12,30 +12,34 @@
 
     if (empty($_POST['music_name'])) {
         echo '曲名を入力してください。';
+        
     }else if(empty($_POST['music_name'])) {
         echo '歌手名を入力してください。';
+
     }else if( $sql->execute([htmlspecialchars($_POST['music_name']), $_POST['artist_name'], $_POST['genre_id'], $_POST['music_id']]) ){
         echo '更新に成功しました！';
-    }
-    else{
+
+    }else{
         echo '更新に失敗しました・・・';
     }
 ?>
-        <hr>
-        <table>
-        <tr><th>楽曲ID</th><th>曲名</th><th>歌手名</th><th>ジャンル</th></tr>
+
+<hr>
+<table>
+<tr><th>楽曲ID</th><th>曲名</th><th>歌手名</th><th>ジャンル</th></tr>
 
 <?php
-foreach ($pdo->query('select * from Music') as $row) {
-    echo '<tr>';
-    echo '<td>', $row['music_id'], '</td>';
-    echo '<td>', $row['music_name'], '</td>';
-    echo '<td>', $row['artist_name'], '</td>';
-    echo '<td>', $row['genre_id'], '</td>';
-    echo '</tr>';
-    echo "\n";
-}
+    foreach ($pdo->query('select * from Music') as $row) {
+        echo '<tr>';
+        echo '<td>', $row['music_id'], '</td>';
+        echo '<td>', $row['music_name'], '</td>';
+        echo '<td>', $row['artist_name'], '</td>';
+        echo '<td>', $row['genre_id'], '</td>';
+        echo '</tr>';
+        echo "\n";
+    }
 ?>
+
 </table>
 
 <?php require 'footer.php'; ?>
